4a25a9838685221d22088c045bb52277c23c25ec
Yury Mayerly Martinez Diaz <yumawis.io@gmail.com>
2026-01-27 11:40:32 -0500

    remove backend from frontend repository

--- backend/src/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { chatController } = require("../controllers/chatController");
-
-router.post("/chat", chatController);
-
-module.exports = router;
